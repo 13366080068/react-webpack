073113adbc50340300168cf8b19384e71aa73287
chenyang <857572418@qq.com>
2019-06-13 14:56:28 +0800

    Finish

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,19 +1,18 @@
+const path = require('path')
+
+const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const PurifyCSS = require('purifycss-webpack')
-const glob = require('glob-all')
+const HappyPack = require('happypack')
+const os = require('os')
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
 
 module.exports = {
-    mode: "development",
-    devtool:"cheap-module-eval-source-map",// 开发环境配置
-    devtool:"cheap-module-source-map",   // 线上生成配置
     entry: ["./src/index.js"],
     output: {
         // 输出目录
-        path: path.join(__dirname, "dist"),
-        // 文件名称
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "../dist")
     },
     resolve: {
         extension: ["", ".js", "jsx"],
@@ -23,27 +22,21 @@ module.exports = {
             router: path.join(__dirname, "src/router")
           }
     },
-    optimization: {
-        usedExports: true,
-        splitChunks: {
-            chunks: "all", // 所有的 chunks 代码公共的部分分离出来成为一个单独的文件
-        }
-    },
     module: {
         rules: [
             {
+                // cnpm i babel-loader @babel/core @babel/preset-env -D
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: "babel-loader"
+                        loader: "happypack/loader?id=happyBabel"
                     }
                 ]
             },
             {
-                test: /\.scss$/,
+                test: /\.(sc|sa|c)ss$/,
                 use: [
-                    "style-loader", // 创建style标签，并将css添加进去
                     MiniCssExtractPlugin.loader,
                     "css-loader", // 编译css
                     "postcss-loader",
@@ -80,9 +73,12 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: 'index.html', // 最终创建的文件名
-            template: path.join(__dirname, 'src/template.html') // 指定模版路径
+            template: path.join(__dirname, 'src/template.html'),
+            minify: {
+                collapseWhitespace: true // 去除空白
+            } // 指定模版路径
         }),
-        new webpack.HotModuleReplacementPlugin(),
+        // css单独提取
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
@@ -91,35 +87,18 @@ module.exports = {
             $: 'jquery', // npm
             jQuery: 'jQuery' // 本地Js文件
         }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                VUEP_BASE_URL: JSON.stringify('http://localhost:9000')
-            }
-        }),
-        // 清除无用 css
-        new PurifyCSS({
-            paths: glob.sync([
-                // 要做 CSS Tree Shaking 的路径文件
-                path.resolve(__dirname, './src/*.html'), // 请注意，我们同样需要对 html 文件进行 tree shaking
-                path.resolve(__dirname, './src/*.js')
-            ])
-        }),
-        new AddAssetHtmlWebpackPlugin({
-            filepath: path.resolve(__dirname, '../dll/jquery.dll.js') // 对应的dll文件路径
-        }),
-        new webpack.DllReferencePlugin({
-            manifest: path.resolve(__dirname, '..', 'dll/jquery-manifest.json')
+        new HappyPack({
+            // 用唯一的标识符id，来代表当前的HappyPack是用来处理一类特定的文件
+            id: 'happyBabel',
+            // 如何处理.js文件，用法和Loader配置中一样
+            loaders: [{
+                loader: 'babel-loader?cacheDirectory=true'
+            }],
+            // 共享进程池threadPool: HappyThreadPool 代表共享进程池，即多个 HappyPack 实例都使用同一个共享进程池中的子进程去处理任务，以防止资源占用过多。
+            threadPool: happyThreadPool,
+            //允许 HappyPack 输出日志
+            verbose: true
         })
     ],
-    devServer: {
-        hot: true,
-        contentBase: path.join(__dirname, "./dist"),
-        host: "0,0,0,0", // 可以使用手机访问
-        port: 8080,
-        historyApiFallback: true, // 该选项的作用所有的404都连接到index.html
-        proxy: {
-            // 代理到后端的服务地址，会拦截所有以api开头的请求地址
-            "/api": "http://localhost:3000"
-        }
-    }
+    performance: false // 关闭性能提示
 }
